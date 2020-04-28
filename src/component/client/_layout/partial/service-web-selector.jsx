92834b4e47f84093a65257e07db096316d46e3e0
Hùng tt <thanhhung.code@gmail.com>
2020-04-29 00:29:52 +0900

    update code icon

---
+++ src/component/client/_layout/partial/service-web-selector.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import "../../../../scss/react/client/page/home.scss";
+
+import BoxClassic from "../../../svg/box-classic.jsx";
+
+class ServiceWebsiteSelector extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="component-service-website-selector">
+        <div className="topic-website-selector">
+          <div className="title-topic">ウェブサイトのデザインニーズ</div>
+        </div>
+        <div className="wrapper-website-selector clear">
+          <div className="block-website-selector">
+            <div className="icon-title-block">
+              <span className="icon-title">
+                <i className="icon-abs-middle hero-icon hero-creation"></i>
+                <BoxClassic fillColor="#DDC2E5" />
+              </span>
+            </div>
+            <h6 className="text-center">ウェブサイトのデザイン</h6>
+            <h4 className="title-block-language">事業</h4>
+            <p className="content-language">ビジネスの開発と促進、ブランド価値の向上</p>
+          </div>
+          <div className="block-website-selector middle">
+            <div className="icon-title-block">
+              <span className="icon-title">
+                <i className="icon-abs-middle hero-icon hero-format-list-text"></i>
+                <BoxClassic fillColor="#FFD5C5" />
+              </span>
+            </div>
+            <h6 className="text-center">ウェブサイトのデザイン</h6>
+            <h4 className="title-block-language">ご自身の要件に応じた</h4>
+            <p className="content-language">アイデアを与えてビジネスに集中し、残りは私たちに任せます</p>
+          </div>
+          <div className="block-website-selector">
+            <div className="icon-title-block">
+            <span className="icon-title">
+              <i className="icon-abs-middle hero-icon hero-auto-fix"></i>
+              <BoxClassic fillColor="#f5d285" />
+            </span>
+            </div>
+            <h6 className="text-center">ウェブサイトのデザイン</h6>
+            <h4 className="title-block-language">電子商取引</h4>
+            <p className="content-language">ビジネスを促進し、オンライン市場を支配する</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ServiceWebsiteSelector;
--- src/component/client/page/Home.jsx
+++ src/component/client/page/Home.jsx
@@ -1,30 +1,60 @@
-import React, { Component } from 'react'
-import Header from "../_layout/header/index.jsx"
-
+import React, { Component } from "react";
+import Header from "../_layout/header/index.jsx";
+import "../../../scss/react/client/page/home.scss";
+import ServiceWebsiteSelector from "../_layout/partial/service-web-selector.jsx";
 
 class Home extends Component {
   constructor(props) {
-
     super(props);
   }
-  
+
   render() {
-    
     return (
       <div className="component-home">
         <Header />
-        <div>home page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, nisi excepturi quae dolorum adipisci iusto at enim nostrum amet unde doloribus, omnis est minima voluptatum quasi. Sed iste delectus cupiditate.</div>
-        <div>home page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, nisi excepturi quae dolorum adipisci iusto at enim nostrum amet unde doloribus, omnis est minima voluptatum quasi. Sed iste delectus cupiditate.</div>
-        <div>home page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, nisi excepturi quae dolorum adipisci iusto at enim nostrum amet unde doloribus, omnis est minima voluptatum quasi. Sed iste delectus cupiditate.</div><div>home page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, nisi excepturi quae dolorum adipisci iusto at enim nostrum amet unde doloribus, omnis est minima voluptatum quasi. Sed iste delectus cupiditate.</div>
-        <div>home page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, nisi excepturi quae dolorum adipisci iusto at enim nostrum amet unde doloribus, omnis est minima voluptatum quasi. Sed iste delectus cupiditate.</div>
-        <div>home page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, nisi excepturi quae dolorum adipisci iusto at enim nostrum amet unde doloribus, omnis est minima voluptatum quasi. Sed iste delectus cupiditate.</div>
-        <div>home page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, nisi excepturi quae dolorum adipisci iusto at enim nostrum amet unde doloribus, omnis est minima voluptatum quasi. Sed iste delectus cupiditate.</div><div>home page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, nisi excepturi quae dolorum adipisci iusto at enim nostrum amet unde doloribus, omnis est minima voluptatum quasi. Sed iste delectus cupiditate.</div>
-        <div>home page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, nisi excepturi quae dolorum adipisci iusto at enim nostrum amet unde doloribus, omnis est minima voluptatum quasi. Sed iste delectus cupiditate.</div>
-        <div>home page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, nisi excepturi quae dolorum adipisci iusto at enim nostrum amet unde doloribus, omnis est minima voluptatum quasi. Sed iste delectus cupiditate.</div>
-        <div>home page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, nisi excepturi quae dolorum adipisci iusto at enim nostrum amet unde doloribus, omnis est minima voluptatum quasi. Sed iste delectus cupiditate.</div>
-        <div>home page Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, nisi excepturi quae dolorum adipisci iusto at enim nostrum amet unde doloribus, omnis est minima voluptatum quasi. Sed iste delectus cupiditate.</div>
+        <ServiceWebsiteSelector />
+        <div>Xây dựng các ứng dụng web động viết trên nền LAMP, LEMP. Có khả năng làm
+        việc ở tất cả các giai đoạn của quy trình phát triển phần mềm từ phân
+        tích hệ thống đến xây dựng website font-end lẫn back-end @include
+        ("genneral.svg.java") Servlet, Java core, C#, Redit JAVA & C# Sử dụng
+        Java, C#,... để built các web chuyên biệt hơn</div>
+        <div>Xây dựng các ứng dụng web động viết trên nền LAMP, LEMP. Có khả năng làm
+        việc ở tất cả các giai đoạn của quy trình phát triển phần mềm từ phân
+        tích hệ thống đến xây dựng website font-end lẫn back-end @include
+        ("genneral.svg.java") Servlet, Java core, C#, Redit JAVA & C# Sử dụng
+        Java, C#,... để built các web chuyên biệt hơn</div>
+        <div>Xây dựng các ứng dụng web động viết trên nền LAMP, LEMP. Có khả năng làm
+        việc ở tất cả các giai đoạn của quy trình phát triển phần mềm từ phân
+        tích hệ thống đến xây dựng website font-end lẫn back-end @include
+        ("genneral.svg.java") Servlet, Java core, C#, Redit JAVA & C# Sử dụng
+        Java, C#,... để built các web chuyên biệt hơn</div>
+        <div>Xây dựng các ứng dụng web động viết trên nền LAMP, LEMP. Có khả năng làm
+        việc ở tất cả các giai đoạn của quy trình phát triển phần mềm từ phân
+        tích hệ thống đến xây dựng website font-end lẫn back-end @include
+        ("genneral.svg.java") Servlet, Java core, C#, Redit JAVA & C# Sử dụng
+        Java, C#,... để built các web chuyên biệt hơn</div>
+        <div>Xây dựng các ứng dụng web động viết trên nền LAMP, LEMP. Có khả năng làm
+        việc ở tất cả các giai đoạn của quy trình phát triển phần mềm từ phân
+        tích hệ thống đến xây dựng website font-end lẫn back-end @include
+        ("genneral.svg.java") Servlet, Java core, C#, Redit JAVA & C# Sử dụng
+        Java, C#,... để built các web chuyên biệt hơn</div>
+        <div>Xây dựng các ứng dụng web động viết trên nền LAMP, LEMP. Có khả năng làm
+        việc ở tất cả các giai đoạn của quy trình phát triển phần mềm từ phân
+        tích hệ thống đến xây dựng website font-end lẫn back-end @include
+        ("genneral.svg.java") Servlet, Java core, C#, Redit JAVA & C# Sử dụng
+        Java, C#,... để built các web chuyên biệt hơn</div>
+        <div>Xây dựng các ứng dụng web động viết trên nền LAMP, LEMP. Có khả năng làm
+        việc ở tất cả các giai đoạn của quy trình phát triển phần mềm từ phân
+        tích hệ thống đến xây dựng website font-end lẫn back-end @include
+        ("genneral.svg.java") Servlet, Java core, C#, Redit JAVA & C# Sử dụng
+        Java, C#,... để built các web chuyên biệt hơn</div>
+        <div>Xây dựng các ứng dụng web động viết trên nền LAMP, LEMP. Có khả năng làm
+        việc ở tất cả các giai đoạn của quy trình phát triển phần mềm từ phân
+        tích hệ thống đến xây dựng website font-end lẫn back-end @include
+        ("genneral.svg.java") Servlet, Java core, C#, Redit JAVA & C# Sử dụng
+        Java, C#,... để built các web chuyên biệt hơn</div>
       </div>
-    )
+    );
   }
 }
 
