84aa630d6f0fe1d1b19b128375ff39d915551416
Trương Thanh hùng <thuong_thanh_hung@managerment-partners.co.jp>
2020-05-22 17:58:35 +0900

    sdfds

--- SERVER/middleware/request/user-register.js
+++ SERVER/middleware/request/user-register.js
@@ -6,7 +6,10 @@ module.exports.VALIDATE_REGISTER = async function( req, res, next ){
     
     let validate = new Validator(req.body, {
         email: 'required|email',
-        password: 'required'
+        password: 'required',
+        head_phone: ['required', 'regex:(+{1}[0-9]{2,3})'],
+        phone : 'required|min:8|max:11',
+        avatar : "required"
     },{
         'email.required': 'hùng đẹp trai'
     });
