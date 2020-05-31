2d83800604e44e4672b13d963602e2692f3316f7
Hùng tt <thanhhung.code@gmail.com>
2020-05-31 14:12:31 +0900

    update send chat

--- FRONTEND/src/action/socket.js
+++ FRONTEND/src/action/socket.js
@@ -1,4 +1,4 @@
-
+var { addMessage } = require("../action")
 var EVENT = CONFIG_EVENT
 exports.socketListenner = function( socket, dispatch ){
 
@@ -8,5 +8,18 @@ exports.socketListenner = function( socket, dispatch ){
     });
     socket.on(EVENT.RESPONSE_MESSAGE, data => {
         console.log("đã vào " + EVENT.RESPONSE_MESSAGE, data)
+        var { user, message, channel } = data 
+        if (typeof(Storage) !== 'undefined') {
+            var userLocal = JSON.parse(localStorage.getItem('user'))
+            if( userLocal && userLocal._id == user ){
+                return false
+            }else{
+                dispatch( addMessage({ type: false, content: message }) )
+                return false
+            }
+        } else {
+            alert('このアプリケーションはこのブラウザをサポートしていません。アップグレードしてください')
+        }
+        
     })
 }
